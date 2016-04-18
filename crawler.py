337dd59897cfd593f4ccc13c1af373cac349ac61
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2016-04-18 10:26:17 +0200

    More readable code.

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import urllib.parse
 
 class Crawler:
     href_regexp = re.compile(r'href=[\'"]?([^\'" >]+)')
@@ -10,27 +11,33 @@ class Crawler:
         self.visited = set()
         self.to_visit = []
 
-    def crawl(self, url=None):
-        try:
-            if url is not None:
-                self.to_visit.append((None, url))
-            while len(self.to_visit) > 0:
-                print('len(visited)  = %d' % len(self.visited))
-                print('len(to_visit) = %d' % len(self.to_visit))
-                print('')
-                origin, target = self.to_visit[0]
-                self.to_visit = self.to_visit[1:]
+    def print_stats(self):
+        print('len(graph)    = %d' % len(self.graph))
+        print('len(visited)  = %d' % len(self.visited))
+        print('len(to_visit) = %d\n' % len(self.to_visit))
+
+
+    def __crawl__(self):
+        while len(self.to_visit) > 0:
+            self.print_stats()
+            origin, target = self.to_visit[0]
+            self.to_visit = self.to_visit[1:]
+            req=requests.get(target)
+            if target not in self.visited:
+                self.visited.add(target)
+                for url in self.url_regexp.findall(req.text):
+                    url = urllib.parse.urlparse(url)
+                    self.to_visit.append((target, '%s://%s' % (url.scheme, url.hostname)))
+                    self.to_visit.append((target, url.geturl()))
                 try:
-                    req=requests.get(target)
-                except Exception:
-                    continue
-                if target not in self.visited:
-                    self.visited.add(target)
-                    for url in self.url_regexp.findall(req.text):
-                        self.to_visit.append((target, url))
-                    try:
-                        self.graph[origin].append(target)
-                    except KeyError:
-                        self.graph[origin] = [target]
+                    self.graph[origin].append(target)
+                except KeyError:
+                    self.graph[origin] = [target]
+
+    def crawl(self, url=None):
+        if url is not None:
+            self.to_visit.append(('/', url))
+        try:
+            self.__crawl__()
         except KeyboardInterrupt:
             return
