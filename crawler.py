a527c098fbe47370d1da1f6ba8ecd4bf1429209e
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2016-04-18 10:39:17 +0200

    Dedicated class to handle the graph.

--- crawler.py
+++ crawler.py
@@ -2,26 +2,39 @@ import requests
 import re
 import urllib.parse
 
+class Graph:
+    def __init__(self):
+        self.in_arcs = dict()
+        self.out_arcs = dict()
+
+    @staticmethod
+    def add_arc(arcs, origin, target):
+        try:
+            arcs[origin].add(target)
+        except KeyError:
+            arcs[origin] = set([target])
+
+    def add(self, origin, target):
+        self.add_arc(self.out_arcs, origin, target)
+        self.add_arc(self.in_arcs, target, origin)
+
 class Crawler:
     href_regexp = re.compile(r'href=[\'"]?([^\'" >]+)')
     url_regexp = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 
     def __init__(self):
-        self.graph = dict()
+        self.graph = Graph()
         self.visited = set()
         self.to_visit = []
 
     def print_stats(self):
-        print('len(graph)    = %d' % len(self.graph))
         print('len(visited)  = %d' % len(self.visited))
         print('len(to_visit) = %d\n' % len(self.to_visit))
-
 
     def __crawl__(self):
         while len(self.to_visit) > 0:
             self.print_stats()
-            origin, target = self.to_visit[0]
-            self.to_visit = self.to_visit[1:]
+            origin, target = self.to_visit.pop(0)
             req=requests.get(target)
             if target not in self.visited:
                 self.visited.add(target)
@@ -29,10 +42,7 @@ class Crawler:
                     url = urllib.parse.urlparse(url)
                     self.to_visit.append((target, '%s://%s' % (url.scheme, url.hostname)))
                     self.to_visit.append((target, url.geturl()))
-                try:
-                    self.graph[origin].append(target)
-                except KeyError:
-                    self.graph[origin] = [target]
+                self.graph.add(origin, target)
 
     def crawl(self, url=None):
         if url is not None:
